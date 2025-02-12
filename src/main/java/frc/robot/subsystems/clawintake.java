051a5e7579801f7e4bde58a7b03fb71a081e04fd
leetucker1 <annaleetucker@icloud.com>
2025-02-12 18:19:25 -0500

    claw base code (?)

---
+++ src/main/java/frc/robot/subsystems/clawintake.java
@@ -0,0 +1,40 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.sim.TalonFXSimState;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+public class clawintake extends SubsystemBase{
+     // All hardware classes already have WPILib integration
+     final TalonFX m_talonFX = new TalonFX(0);
+     final CANcoder m_cancoder = new CANcoder(0);
+ 
+     final TalonFXSimState m_talonFXSim = m_talonFX.getSimState();
+ 
+     final DutyCycleOut m_talonFXOut = new DutyCycleOut(0);
+ 
+     final TalonFXConfiguration m_talonFXConfig = new TalonFXConfiguration();
+     final CANcoderConfiguration m_cancoderConfig = new CANcoderConfiguration();
+ 
+     InvertedValue m_talonFXInverted = InvertedValue.CounterClockwise_Positive;
+
+
+
+     public clawintake(){
+
+        m_talonFX.setControl(m_talonFXOut);
+        // motor configuartion section
+
+     }
+ 
+
+    
+}
