5562210ecd3a18dfa51190cf997443e60d84d51f
Matthew112009 <msiswaja11@gmail.com>
2025-02-12 19:16:29 -0500

    update to elevator code

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -3,47 +3,55 @@ package frc.robot.subsystems;
 import com.ctre.phoenix6.configs.CANcoderConfiguration;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.sim.TalonFXSimState;
 
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 
 public class Elevator extends SubsystemBase{
     // All hardware classes already have WPILib integration
-    final TalonFX m_talonFX = new TalonFX(0);
-    final CANcoder m_cancoder = new CANcoder(0);
+    final TalonFX m_elevator_motor = new TalonFX(21);
+    
 
-    final TalonFXSimState m_talonFXSim = m_talonFX.getSimState();
+    final TalonFXSimState m_elevator_motorSim = m_elevator_motor.getSimState();
 
-    final DutyCycleOut m_talonFXOut = new DutyCycleOut(0);
+    final PositionVoltage m_elevator_motorOut = new PositionVoltage(0);
 
-    final TalonFXConfiguration m_talonFXConfig = new TalonFXConfiguration();
-    final CANcoderConfiguration m_cancoderConfig = new CANcoderConfiguration();
-
-    InvertedValue m_talonFXInverted = InvertedValue.CounterClockwise_Positive;
+    final TalonFXConfiguration m_elevator_motorConfig = new TalonFXConfiguration();
+    
+    
 
 
     
     public Elevator(){
+        m_elevator_motorConfig.Slot0(
+            //PID
+        )
         
-        m_talonFX.setControl(m_talonFXOut);
+        
         // motor configuration section
+        m_elevator_motor.getConfigurator().apply(m_elevator_motorConfig);
+
 
     }
     
 
     public void set_elevator_position(Double posision){
-        //motor contolling section
-        // motor.set_position(position)
-
-    }
-       
+        
+           
+            }
+        
+        
     public Command get_posiCommand(Double position ){
         return run(()->set_elevator_position(position));
     }
 
 }
+
